63111f82ca558a9174e581c22b56531bdbc97df2
tommyleo <137581768@qq.com>
2019-06-09 18:33:02 +0800

    Remove TCPServer, considering use composition to enable network feature.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-#include "framework/tcp_server.hpp"
+#include <iostream>
+#include "core/io_server.hpp"
+
+class TestServer : public AServer::Core::IOServer {
+  public:
+    void onStart() override {
+        auto handle = this->getTimer()->repeat(
+            0.5, []() { std::cout << "Hello world" << std::endl; });
+        this->getTimer()->once(
+            2.1, [handle(std::move(handle))]() { handle.lock()->cancel(); });
+    }
+
+    void onEnd() override {}
+};
 
 int main() {
-    AServer::Framework::TCPServer server;
+    TestServer server;
     server.run();
 }
